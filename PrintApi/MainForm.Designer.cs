aa83a9b7244adb2c3f68192d28dc86a3b887152f
jdjeong <jdjeong@exi4cns.co.kr>
2022-07-05 14:05:51 +0900

    초기 커밋.

---
+++ PrintApi/MainForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TestProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(100, 21);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 30);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(100, 75);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(653, 79);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "제브라 프린터 구동프로그램입니다. ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "상태";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "결과";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(100, 190);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(653, 155);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "실행 전입니다.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(784, 371);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.startButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Name = "MainForm";
+            this.Text = "프린터 출력 프로그램";
+            this.Load += new System.EventHandler(this.startButton_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ PrintApi/MainForm.cs
@@ -0,0 +1,392 @@
+﻿using MesSystem;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Ghostscript.NET.Processor;
+using System.IO;
+using Serilog;
+
+namespace TestProject
+{
+
+    
+
+    /// <summary>
+    /// 메인 폼
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        //////////////////////////////////////////////////////////////////////////////////////////////////// Field
+        ////////////////////////////////////////////////////////////////////////////////////////// Private
+
+        #region Field
+
+        /// <summary>
+        /// 서버
+        /// </summary>
+        private WebServer server = null;
+
+        private HttpListener httpListener;
+
+        public object ThreadPool { get; private set; }
+
+        #endregion
+
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////// Constructor
+        ////////////////////////////////////////////////////////////////////////////////////////// Public
+
+        #region 생성자 - MainForm()
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+
+
+            this.server = new WebServer();
+
+            this.server.AddBindingAddress("http://localhost:9999/"); // 포트 번호까지 반드시 설정합니다.
+
+            this.server.RootPath = "c:\\wwwroot";
+
+            this.server.ActionRequested += server_ActionRequested;
+
+
+            FormClosing += Form_FormClosing;
+
+            this.startButton.Click += startButton_Click;
+
+
+            
+        }
+
+        #endregion
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////// Method
+        ////////////////////////////////////////////////////////////////////////////////////////// Private
+
+        private void Main_Load(object sender, System.EventArgs e)
+        {
+            //자동실행
+            this.Activated += startButton_Click;
+
+            
+        }
+
+
+        #region 폼을 닫을려는 경우 처리하기 - Form_FormClosing(sender, e)
+
+        /// <summary>
+        /// 폼을 닫을려는 경우 처리하기
+        /// </summary>
+        /// <param name="sender">이벤트 발생자</param>
+        /// <param name="e">이벤트 인자</param>
+        private void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(this.server.IsRunning)
+            {
+                this.server.Stop();
+
+                this.server = null;
+            }
+        }
+
+        #endregion
+        #region Start 버튼 클릭시 처리하기 - startButton_Click(sender, e)
+
+        /// <summary>
+        /// Start 버튼 클릭시 처리하기
+        /// </summary>
+        /// <param name="sender">이벤트 발생자</param>
+        /// <param name="e">이벤트 인자</param>
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if(this.server.IsRunning)
+            {
+                this.server.Stop();
+
+                this.startButton.Text = "Start";
+
+                this.richTextBox1.Text = "제브라 프린터 시스템이 종료되었습니다.";
+
+                this.richTextBox2.Text = "프린터 출력 대기상태가 아닙니다. Start를 눌러 실행시켜주세요.";
+            }
+            else
+            {
+
+
+                this.server.Start();
+                
+                serverInit();
+
+                this.startButton.Text = "Stop";
+
+                this.richTextBox1.Text = "제브라 프린터 시스템이 구동중입니다.";
+
+                this.richTextBox2.Text = "프린터 출력 대기중입니다.";
+
+            }
+        }
+
+        #endregion
+
+        private void serverInit()
+        {
+            if (httpListener == null)
+            {
+                //API 세팅 주소
+                httpListener = new HttpListener();
+                httpListener.Prefixes.Add(string.Format("http://localhost:8585/"));                
+                //httpListener.Prefixes.Add(string.Format("http://+:80/"));
+                serverStart();
+            }
+        }
+
+        private void serverStart()
+        {
+
+            if (!httpListener.IsListening)
+            {
+                httpListener.Start();                
+
+                Task.Factory.StartNew(() =>
+                {
+                    while (httpListener != null)
+                    {
+
+                        HttpListenerContext ctx = this.httpListener.GetContext();
+
+                        //string methodName = ctx.Request.Url.Segments[1].Replace("/", "");
+                        //string strParams = System.Net.WebUtility.UrlEncode(ctx.Request.Url.Segments[2].Replace("/", ""));
+
+                        string strParams = ctx.Request.Url.LocalPath.Replace("/param=","");
+
+                        PrintDialog pd = new PrintDialog();
+                        pd.PrinterSettings = new PrinterSettings();
+
+                        PrinterSettings oPrinterSettings = new PrinterSettings();
+
+                        string printerName = oPrinterSettings.PrinterName;
+
+                        //USER 프린터일 경우
+                        if (strParams.Contains("pdf"))
+                        {
+                            string inputFile = @"D:\D365_PRINT\PDF\" + strParams;
+
+                            using (GhostscriptProcessor processor = new GhostscriptProcessor())
+                            {
+                                List<string> switches = new List<string>();
+                                switches.Add("-empty");
+                                switches.Add("-dPrinted");
+                                switches.Add("-dBATCH");
+                                switches.Add("-dNOPAUSE");
+                                //switches.Add("-dQUIET");
+                                switches.Add("-dNOSAFER");
+                                switches.Add("-dNoCancel");
+                                switches.Add("-dNumCopies=1");
+                                switches.Add("-dQueryUser=3");
+                                switches.Add("-dORIENT1=false");
+                                //switches.Add("-sPAPERSIZE=a4");
+
+                                //switches.Add("-dNORANGEPAGESIZE");
+                                switches.Add("-sDEVICE=mswinpr2");
+                                switches.Add("-sOutputFile=%printer%" + printerName);
+                                switches.Add("-f");
+                                switches.Add(inputFile);
+
+
+                                try
+                                {
+                                    processor.StartProcessing(switches.ToArray(), null);
+                                    CreateFileSerilog("ZPL|" + printerName + "|" + inputFile);
+                                }
+                                catch (Exception e) {
+                                    CreateFileSerilog("ZPL_ERROR|" + e.Message.ToString());
+                                }
+                                finally
+                                {
+
+                                    if (richTextBox2.InvokeRequired)
+                                        richTextBox2.Invoke(new MethodInvoker(delegate { richTextBox2.Text = "출력 성공"; }));
+                                }
+
+                            }
+
+
+                            //60일 지난 파일 삭제
+                            try
+                            {
+                                Delete_File("PDF");                                
+                            }
+                            catch (Exception e)
+                            {
+                                CreateFileSerilog("ZPL_FILEDEL_ERROR|" + e.Message.ToString());
+                            }
+
+
+                        }
+                        else { //제브라 프린터
+
+                            dynamic dynJson = JsonConvert.DeserializeObject(strParams);
+                            string result = string.Empty;
+                            string barcode = string.Empty;
+                            foreach (var item in dynJson)
+                            {
+
+                                result += string.Format("rawurl = {0}\r\n", item.TEXT1);
+                                result += string.Format("rawurl = {0}\r\n", item.TEXT2);
+
+                                barcode += this.getbarcode(Convert.ToString(item.TEXT1), Convert.ToString(item.TEXT2));
+                            }
+
+
+                            if (richTextBox1.InvokeRequired)
+                                richTextBox1.Invoke(new MethodInvoker(delegate { richTextBox1.Text = result; }));
+                            else
+                                richTextBox1.Text = result;
+
+
+                            try
+                            {
+                                RawPrinterHelper.SendStringToPrinter(pd.PrinterSettings.PrinterName, barcode);
+
+                                CreateFileSerilog("PRINT|" + pd.PrinterSettings.PrinterName + "|" + barcode);
+
+                            }
+                            catch (Exception e)
+                            {
+
+                                if (richTextBox2.InvokeRequired)
+                                    richTextBox2.Invoke(new MethodInvoker(delegate { richTextBox2.Text = e.Message.ToString(); }));                               
+                                
+                                CreateFileSerilog("PRINT_ERROR|" + e.Message.ToString());
+                            }
+                            finally
+                            {
+
+                                if (richTextBox2.InvokeRequired)
+                                    richTextBox2.Invoke(new MethodInvoker(delegate { richTextBox2.Text = "출력 성공"; }));     
+                                
+                            }
+
+
+                            ctx.Response.Close();
+
+                            //60일 지난 로그 파일 삭제
+                            Delete_File("LOG");
+
+                        }
+
+                    }
+                });
+
+            }
+        }
+
+        
+
+        /// <summary>바코드 출력</summary>
+        private string getbarcode(string text1, string text2)
+        {
+            string BarCode = string.Empty;            
+
+            BarCode = "^XA";
+            BarCode = BarCode + "^FX BOX DRAW";
+            BarCode = BarCode + "^FO10,40^GB530,290,3^FS";
+            BarCode = BarCode + "^FO10,40^GB530,335,3^FS";
+
+            BarCode = BarCode + "^FX ITEMID AND Sn";
+            BarCode = BarCode + "^CFA,30 ";
+
+            BarCode = BarCode + "^FX ITEM CD BARCODE (VALUE)";
+            BarCode = BarCode + "^BY3,2,90";
+            BarCode = BarCode + "^FO20,50^BC,126,N^" + text1 + "^FS";
+
+            BarCode = BarCode + "^FX ITEM CD TEXT (FIX)";
+            BarCode = BarCode + "^FO20,150^FDITEM CD :^FS";
+
+            BarCode = BarCode + "^FX SN BARCODE(VALUE)";
+            BarCode = BarCode + "^BY3,2,90";
+            BarCode = BarCode + "^FO20,190^BC^FD" + text2 + "^FS";
+
+
+            BarCode = BarCode + "^FX SN BARCODE(VALUE)";
+            BarCode = BarCode + "^BY3,2,90";
+            BarCode = BarCode + "^FO20,190^BC^" + text2 + "^FS";
+
+            BarCode = BarCode + "^FX SN TEXT (FIX)";
+            BarCode = BarCode + "^FO20,290^FDSN :^FS ";
+
+            BarCode = BarCode + "^FX SN TEXT (VALUE)";
+            BarCode = BarCode + "^FO100,290^FD" + text2 + "^FS";
+
+            BarCode = BarCode + "^CF0,30,30";
+            BarCode = BarCode + "^FO70,340^FDMANUFACTURED BY TYM. KOREA^FS";
+            BarCode = BarCode + "^XZ";
+
+            return BarCode;
+            
+        }
+
+        #region 60일 지난  파일 삭제
+
+        private void Delete_File(string path) {
+
+            string deletePath = @"D:\D365_PRINT/" + path;
+
+            (from f in new DirectoryInfo(deletePath).GetFiles()
+             where f.CreationTime < DateTime.Now.Subtract(TimeSpan.FromDays(60))
+             select f
+            ).ToList()
+                .ForEach(f => f.Delete());
+        }
+
+        #endregion
+
+
+        #region Log 기록
+        //Log 파일에 기록
+        private static void CreateFileSerilog(string logTxt)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File(@"D:\D365_PRINT/LOG/log.txt",
+                rollingInterval: RollingInterval.Day,
+                rollOnFileSizeLimit: true)
+            .CreateLogger();
+
+            Log.Information(logTxt);
+ 
+
+            Log.CloseAndFlush();
+        }
+        #endregion
+
+        #region 서버 액션 요청시 처리하기 - server_ActionRequested(sender, e)
+
+        /// <summary>
+        /// 서버 액션 요청시 처리하기
+        /// </summary>
+        /// <param name="sender">이벤트 발생자</param>
+        /// <param name="e">이벤트 인자</param>
+        private void server_ActionRequested(object sender, ActionRequestedEventArgs e)
+        {
+            e.Server.WriteDefaultAction(e.Context);
+        }
+
+        #endregion
+    }
+}
